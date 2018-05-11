c4e38cf060e1fe64967e27ca0fedf761908a7288
Frederico Gendorf <fred@esss.co>
2018-05-11 13:49:27 -0300

    Versão 1.0.5 bugfix

--- code/REPRegistros.php
+++ code/REPRegistros.php
@@ -46,9 +46,9 @@ class REPRegistros extends REPHenry {
      * @return array
      */
     public function getInclusaoAlteracaoEmpresa($registros) {
+        $y=-1;
         for ($x = 0; sizeof($registros) > $x; ++$x) {
             if (substr($registros[$x], 9, 1) == 2) {
-                ++$y;
                 $regarray[$y]["NSR"] = substr($registros[$x], 0, 9);
                 $regarray[$y]["tipo"] = substr($registros[$x], 9, 1);
                 $regarray[$y]["data"] = substr($registros[$x], 10, 8);
@@ -58,6 +58,7 @@ class REPRegistros extends REPHenry {
                 $regarray[$y]["cei"] = substr($registros[$x], 37, 12);
                 $regarray[$y]["razaosocial"] = substr($registros[$x], 49, 150);
                 $regarray[$y]["local"] = substr($registros[$x], 199, 100);
+                ++$y;
             }
         }
         return $regarray;
@@ -70,14 +71,15 @@ class REPRegistros extends REPHenry {
      * @return array
      */
     public function getMarcacaoPonto($registros) {
+        $y=-1;
         for ($x = 0; sizeof($registros) > $x; ++$x) {
             if (substr($registros[$x], 9, 1) == 3) {
-                ++$y;
                 $regarray[$y]["NSR"] = substr($registros[$x], 0, 9);
                 $regarray[$y]["tipo"] = substr($registros[$x], 9, 1);
                 $date = DateTime::createFromFormat("dmYHi", substr($registros[$x], 10, 8) . substr($registros[$x], 18, 4));
                 $regarray[$y]["datahorario"] = $date->format("Y-m-d H:i:s");
                 $regarray[$y]["pis"] = substr($registros[$x], 22, 12);
+                ++$y;
             }
         }
         return $regarray;
@@ -90,14 +92,15 @@ class REPRegistros extends REPHenry {
      * @return array
      */
     public function getMarcacaoPontoMatricula($registros) {
+        $y=-1;
         for ($x = 0; sizeof($registros) > $x; ++$x) {
             if (substr($registros[$x], 9, 1) == 7) {
-                ++$y;
                 $regarray[$y]["NSR"] = substr($registros[$x], 0, 9);
                 $regarray[$y]["tipo"] = substr($registros[$x], 9, 1);
                 $date = DateTime::createFromFormat("dmYHi", substr($registros[$x], 10, 8) . substr($registros[$x], 18, 4));
                 $regarray[$y]["datahorario"] = $date->format("Y-m-d H:i:s");
                 $regarray[$y]["matricula"] = substr($registros[$x], -6);
+                ++$y;
             }
         }
         return $regarray;
@@ -110,15 +113,16 @@ class REPRegistros extends REPHenry {
      * @return array
      */
     public function getAjusteRelogio($registros) {
+        $y=-1;
         for ($x = 0; sizeof($registros) > $x; ++$x) {
             if (substr($registros[$x], 9, 1) == 4) {
-                ++$y;
                 $regarray[$y]["NSR"] = substr($registros[$x], 0, 9);
                 $regarray[$y]["tipo"] = substr($registros[$x], 9, 1);
                 $regarray[$y]["dataantes"] = substr($registros[$x], 10, 8);
                 $regarray[$y]["horarioantes"] = substr($registros[$x], 18, 4);
                 $regarray[$y]["dataajustada"] = substr($registros[$x], 22, 8);
                 $regarray[$y]["horaajustada"] = substr($registros[$x], 30, 4);
+                ++$y;
             }
         }
         return $regarray;
@@ -132,15 +136,16 @@ class REPRegistros extends REPHenry {
      * @return array
      */
     public function getInclusaoAlteracaoEmpregado($registros) {
+        $y=-1;
         for ($x = 0; sizeof($registros) > $x; ++$x) {
             if (substr($registros[$x], 9, 1) == 5) {
-                ++$y;
                 $regarray[$y]["NSR"] = substr($registros[$x], 0, 9);
                 $regarray[$y]["tipo"] = substr($registros[$x], 9, 1);
                 $regarray[$y]["data"] = substr($registros[$x], 10, 12);
                 $regarray[$y]["acao"] = substr($registros[$x], 22, 1);
                 $regarray[$y]["pis"] = substr($registros[$x], 23, 12);
                 $regarray[$y]["colaborador"] = substr($registros[$x], 35, 52);
+                ++$y;
             }
         }
         return $regarray;
